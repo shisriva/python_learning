b676fafa8b22528c7b66e0ded2690a82829ee4f0
shisriva <shivam.nss@gmail.com>
2026-07-24 20:43:29 +0530

    gpt_challenge_class

--- class.py
+++ class.py
@@ -1,3 +1,36 @@
-class Test:
-    def __init__(self):
-        pass
+'''
+Create a class called Dog.
+
+Requirements:
+
+Attributes:
+name
+age
+Method:
+bark()
+
+Output:
+
+Dog Name: Bruno
+Age: 3
+Bruno says Woof!
+
+
+'''
+
+
+
+
+class Dog:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+        
+        
+    def bark(self):
+        print(f'Dog Name: {self.name}\n Age: {self.age} \n {self.name} says Woof!')
+        
+        
+my_dog = Dog('Bruno', 3)
+
+my_dog.bark
